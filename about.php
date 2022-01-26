5ebaebf8362389706d6d86234dfbac4612c44d25
Ben Southwick <blsout23@colby.edu>
2022-01-26 02:08:49 +0000

    Adding footage page and making phps.

---
+++ about.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Getting Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Damion&family=Varela+Round&display=swap" rel="stylesheet"> 
+    
+    <link rel="stylesheet" href="css/master.css">
+    <link rel="stylesheet" href="css/about.css">
+    <title>Blue Lights</title>
+</head>
+<body>
+    <?php
+        echo file_get_contents("header.html");
+    ?>
+    
+    <section class="box">
+        <h2>About the Blue Lights</h2>
+        <img class="groupPic" alt="group picture" src="css/media/group1.JPG">
+        <div class="boxText">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Integer feugiat scelerisque varius morbi enim nunc. Mauris pharetra et ultrices neque ornare aenean euismod elementum. Non sodales neque sodales ut etiam. Amet venenatis urna cursus eget nunc scelerisque viverra. Sagittis nisl rhoncus mattis rhoncus urna neque. Volutpat diam ut venenatis tellus in metus. Laoreet id donec ultrices tincidunt. Et magnis dis parturient montes. Posuere morbi leo urna molestie. Elementum curabitur vitae nunc sed velit dignissim sodales. Nulla facilisi etiam dignissim diam quis enim. Pellentesque adipiscing commodo elit at imperdiet dui accumsan sit. Etiam non quam lacus suspendisse faucibus interdum. Lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit.</p>
+
+            <p>Nec sagittis aliquam malesuada bibendum arcu vitae elementum curabitur vitae. Quis commodo odio aenean sed adipiscing. Diam vel quam elementum pulvinar etiam. Pellentesque habitant morbi tristique senectus. Neque viverra justo nec ultrices dui sapien eget mi proin. Diam phasellus vestibulum lorem sed risus ultricies. Ultrices neque ornare aenean euismod. Pretium quam vulputate dignissim suspendisse in est ante in. Aliquam vestibulum morbi blandit cursus risus at. Ac ut consequat semper viverra nam libero justo laoreet. Aliquam sem fringilla ut morbi tincidunt augue interdum velit. Ac orci phasellus egestas tellus rutrum tellus pellentesque eu. Sapien faucibus et molestie ac feugiat sed lectus vestibulum. Felis bibendum ut tristique et egestas quis. Volutpat commodo sed egestas egestas fringilla phasellus faucibus. Libero justo laoreet sit amet cursus. Tempor nec feugiat nisl pretium. Massa sapien faucibus et molestie ac feugiat sed lectus. Mi quis hendrerit dolor magna eget.</p>
+        </div>
+    </section>
+
+    <div class="footerSpacer"></div>
+
+    <?php
+        echo file_get_contents("footer.html");
+    ?>  
+</body>
+</html>
